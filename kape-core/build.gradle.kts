834bd674147a1a97abc367e880e5903dbdd37ef6
AtSignRANK <hino123456@outlook.kr>
2022-08-28 23:48:32 +0900

    publishing-test

---
+++ kape-core/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `maven-publish`
+    signing
+}
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    create<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+        from("$buildDir/dokka/html")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(project.name) {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+}
