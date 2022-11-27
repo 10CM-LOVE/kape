30d9e96bae60b4319de71c64204192b2b470402e
AtSignRANK <hino123456@outlook.kr>
2022-11-27 11:40:08 +0900

    * later bug

--- kape-core/src/main/kotlin/io/github/asr/kape/schedule/Scheduler.kt
+++ kape-core/src/main/kotlin/io/github/asr/kape/schedule/Scheduler.kt
@@ -3,7 +3,7 @@ package io.github.asr.kape.schedule
 import org.bukkit.plugin.Plugin
 
 fun Plugin.later(tick: Long, block: () -> Unit) =
-    server.scheduler.runTaskTimer(this, Runnable(block), tick, 0L)
+    server.scheduler.runTaskLater(this, Runnable(block), tick)
 
 fun Plugin.loop(term: Long, times: Int, block: () -> Unit) {
     val id = server.scheduler.scheduleSyncRepeatingTask(this, block, 0L, term)
